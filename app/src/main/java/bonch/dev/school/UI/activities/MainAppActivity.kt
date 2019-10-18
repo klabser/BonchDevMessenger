e400d140ce1d3e5e46985409dbab7badb6be63bb
klabser <klabser@gmail.com>
2019-10-18 17:46:01 +0300

    Start!

--- app/src/main/java/bonch/dev/school/UI/activities/MainAppActivity.kt
+++ app/src/main/java/bonch/dev/school/UI/activities/MainAppActivity.kt
@@ -1,12 +1,12 @@
-package bonch.dev.school
+package bonch.dev.school.UI.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainAppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_app)
     }
 }
---
+++ app/src/main/java/bonch/dev/school/UI/fragments/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package bonch.dev.school.UI.fragments
+
+import androidx.fragment.app.Fragment
+
+class ProfileFragment: Fragment {
+
+}
